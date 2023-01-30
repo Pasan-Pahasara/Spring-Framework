098f1884fae50fc70abff5d1e4bb68dda8e36375
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-01-30 22:30:44 +0530

    Introducing FullMode_LightMode Module

---
+++ 05_FullMode_LightMode/src/lk/ijse/spring/config/AppConfig.java
@@ -0,0 +1,15 @@
+package lk.ijse.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author : ShEnUx
+ * @time : 10:24 PM
+ * @date : 1/30/2023
+ * @since : 0.1.0
+ **/
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.spring")
+public class AppConfig {
+}
---
+++ 05_FullMode_LightMode/src/lk/ijse/spring/pojo/PojoOne.java
@@ -0,0 +1,16 @@
+package lk.ijse.spring.pojo;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : ShEnUx
+ * @time : 10:25 PM
+ * @date : 1/30/2023
+ * @since : 0.1.0
+ **/
+@Component
+public class PojoOne {
+    public PojoOne() {
+        System.out.println("PojoOne:Instantiated");
+    }
+}
