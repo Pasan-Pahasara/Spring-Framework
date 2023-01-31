9855ccf780774c946ad6ba4e10dcfa8b54097586
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-01-31 17:10:18 +0530

    Introducing @Value annotation

--- 07_Runtime_Value_Injection/src/lk/ijse/spring/pojo/Customer.java
+++ 07_Runtime_Value_Injection/src/lk/ijse/spring/pojo/Customer.java
@@ -1,5 +1,6 @@
 package lk.ijse.spring.pojo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
@@ -10,7 +11,11 @@ import org.springframework.stereotype.Component;
  **/
 @Component
 public class Customer {
-    public Customer() {
-        System.out.println("Customer:Instantiated");
+//    public Customer() {
+//        System.out.println("Default Constructor:Customer:Instantiated");
+//    }
+    public Customer(@Value("IJSE") String name) {
+        System.out.println("Customer:Instantiated "+name);
     }
+
 }
