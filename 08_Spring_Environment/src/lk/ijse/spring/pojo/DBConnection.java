d8b7876b8b953a9b9642a6c191eccf56f7cf0370
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-01-31 18:54:45 +0530

    Added Spring Environment Module

---
+++ 08_Spring_Environment/src/lk/ijse/spring/pojo/DBConnection.java
@@ -0,0 +1,16 @@
+package lk.ijse.spring.pojo;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : ShEnUx
+ * @time : 6:49 PM
+ * @date : 1/31/2023
+ * @since : 0.1.0
+ **/
+@Component
+public class DBConnection {
+    public DBConnection() {
+        System.out.println("DBConnection:Instantiated");
+    }
+}
