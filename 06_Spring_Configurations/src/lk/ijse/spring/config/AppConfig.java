15f8874dd650a46a0516f8957adb5ab0b08f7e15
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-01-31 12:15:14 +0530

    Introducing Spring Configurations Module

---
+++ 06_Spring_Configurations/src/lk/ijse/spring/config/AppConfig.java
@@ -0,0 +1,15 @@
+package lk.ijse.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author : ShEnUx
+ * @time : 12:09 PM
+ * @date : 1/31/2023
+ * @since : 0.1.0
+ **/
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.spring")
+public class AppConfig {
+}
---
+++ 06_Spring_Configurations/src/lk/ijse/spring/pojo/PojoOne.java
@@ -0,0 +1,16 @@
+package lk.ijse.spring.pojo;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : ShEnUx
+ * @time : 12:12 PM
+ * @date : 1/31/2023
+ * @since : 0.1.0
+ **/
+@Component
+public class PojoOne {
+    public PojoOne(){
+        System.out.println("PojoOne:Instantiated");
+    }
+}
