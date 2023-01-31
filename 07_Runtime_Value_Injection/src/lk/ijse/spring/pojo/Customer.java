40e2466a614735a5ca27fee494fddf43eddd4cc4
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-01-31 17:16:38 +0530

    Content Updated with @Value annotation

--- 07_Runtime_Value_Injection/src/lk/ijse/spring/pojo/Customer.java
+++ 07_Runtime_Value_Injection/src/lk/ijse/spring/pojo/Customer.java
@@ -14,8 +14,18 @@ public class Customer {
 //    public Customer() {
 //        System.out.println("Default Constructor:Customer:Instantiated");
 //    }
-    public Customer(@Value("IJSE") String name) {
-        System.out.println("Customer:Instantiated "+name);
+//    public Customer(@Value("IJSE") String name) {
+//        System.out.println("Customer:Instantiated "+name);
+//    }
+    public Customer(@Value("Dasun,Kasun,Amal") String names[]) {
+        System.out.println("Customer:Instantiated");
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
+    //what you can assign with @Value annotation
+    //Primitive Data Types
+    //String data
+    //Arrays
 
 }
