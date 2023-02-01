2852ba79197c8ab93e3f50397878dc1f284e01f1
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-01 17:46:53 +0530

    Content Updated With Mapping Spec Module

---
+++ 12_Mapping_Spec/src/lk/ijse/spring/controller/OneController.java
@@ -0,0 +1,15 @@
+package lk.ijse.spring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : ShEnUx
+ * @time : 5:45 PM
+ * @date : 2/1/2023
+ * @since : 0.1.0
+ **/
+@RestController
+@RequestMapping("/one")
+public class OneController {
+}
