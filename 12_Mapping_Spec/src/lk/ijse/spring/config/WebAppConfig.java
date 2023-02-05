68126abfa16a91724bbc6fe775c62e08b8d612fb
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-05 12:48:53 +0530

    Refactored Update Content

--- 12_Mapping_Spec/src/lk/ijse/spring/config/WebAppConfig.java
+++ 12_Mapping_Spec/src/lk/ijse/spring/config/WebAppConfig.java
@@ -1,6 +1,6 @@
 package lk.ijse.spring.config;
 
-import lk.ijse.spring.controller.CustomerController;
+import lk.ijse.spring.controller.AController;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -13,6 +13,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
  **/
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackageClasses = {CustomerController.class})
+@ComponentScan(basePackageClasses = {AController.class})
 public class WebAppConfig {
 }
