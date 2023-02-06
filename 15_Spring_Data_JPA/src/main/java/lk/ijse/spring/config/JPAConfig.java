c1021b7d27f26a64d6fd643abaf111805a3ce0ec
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-06 15:28:25 +0530

    Added JPAConfig Configuration Class and import it to the WebRootConfig

--- 15_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ 15_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -1,15 +1,15 @@
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 /**
  * @author : ShEnUx
- * @time : 5:21 PM
- * @date : 2/1/2023
+ * @time : 3:25 PM
+ * @date : 2/6/2023
  * @since : 0.1.0
  **/
 @Configuration
-@Import(JPAConfig.class)
-public class WebRootConfig {
+public class JPAConfig {
+
 }
