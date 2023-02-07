5d373030901d185918635862e59ca5b9cb68d4bf
Pasan-Pahasara <pasanpahasara7788@gmail.com>
2023-02-07 10:11:52 +0530

    Introducing PurchaseOrderController

---
+++ 15_Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/PurchaseOrderController.java
@@ -0,0 +1,20 @@
+package lk.ijse.spring.controller;
+
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author : ShEnUx
+ * @time : 10:05 AM
+ * @date : 2/7/2023
+ * @since : 0.1.0
+ **/
+@RestController
+@RequestMapping("/purchase")
+@CrossOrigin
+public class PurchaseOrderController {
+    @PostMapping
+    public ResponseUtil purchaseOrder(@RequestBody){
+        return new ResponseUtil("OK","Purchased Successfully ..!",null);
+    }
+}
